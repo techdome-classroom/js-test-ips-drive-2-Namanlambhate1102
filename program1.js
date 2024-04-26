84bb6ea7add4a0d66985320d631c62b1319ddffc
Namanlambhate1102 <168165532+Namanlambhate1102@users.noreply.github.com>
2024-04-26 16:45:27 +0530

    completed program1.js

--- program1.js
+++ program1.js
@@ -1,22 +1,29 @@
 var isValid = function(s) {
-  const stack = [];
-  const pairs = {
-      '(': ')',
-      '[': ']',
-      '{': '}'
-  };
-  for (let i = 0; i < s.length; i++) {
-      const char = s[i];
-      if (pairs[char]) {
-          stack.push(char);
-      } else {
-          const lastOpening = stack.pop();
-          if (pairs[lastOpening] !== char) {
-              return false;
-          }
-      }
-  }
-  return stack.length === 0;
+    const stack = [];
+    const pairs = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (pairs[char]) {
+            stack.push(char);
+        } else {
+            // Check if the stack is empty before popping
+            if (stack.length === 0) {
+                return false;
+            }
+            const lastOpening = stack.pop();
+            if (pairs[lastOpening] !== char) {
+                return false;
+            }
+        }
+    }
+
+    // Check if there are remaining unclosed parentheses in the stack
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
