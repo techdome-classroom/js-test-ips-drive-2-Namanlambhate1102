c802fb45bf0a05246e2ca933b09c0423be7ae5b0
Namanlambhate1102 <168165532+Namanlambhate1102@users.noreply.github.com>
2024-04-26 16:08:42 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,10 +1,23 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    
-};
+function longestSubstring(s) {
+    let mxlen = 0;
+    let left = 0;
+    const charMap = new Map();  
+
+    for (let right = 0; right < s.length; right++) {
+        const currChar = s[right];
+
+         
+        if (charMap.has(currChar) && charMap.get(currChar) >= left) {
+            left = charMap.get(currChar) + 1;
+        }
+
+         charMap.set(currChar, right);
+
+         mxlen = Math.max(mxlen, right - left + 1);
+    }
+
+    return mxlen;
+}
 
 
-module.exports={romanToInt}
+module.exports = { longestSubstring };
