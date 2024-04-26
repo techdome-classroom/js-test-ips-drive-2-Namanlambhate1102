5c07823590d4317b8fca531fa78d874a7a480215
Namanlambhate1102 <168165532+Namanlambhate1102@users.noreply.github.com>
2024-04-26 16:17:26 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,31 +1,14 @@
-function smallestMissingPositiveInteger(nums) {
-  const n = nums.length;
-  
-   let j = 0;
-  for (let i = 0; i < n; i++) {
-      if (nums[i] <= 0) {
-          [nums[i], nums[j]] = [nums[j], nums[i]];
-          j++;
-      }
+var isValid = function(s) {
+  const stack = [];
+  const characters = { ')': '(', '}': '{', ']': '[' };
+  for (const char of s) {
+    if (!characters[char]) {
+      stack.push(char);
+    } else if (stack.pop() !== characters[char]) {
+      return false;
+    }
   }
-  
-   const x = nums.slice(j);
-  
-   for (let i = 0; i < x.length; i++) {
-      const index = Math.abs(x[i]) - 1;
-      if (index < x.length && x[index] > 0) {
-          x[index] = -x[index];
-      }
-  }
-  
-   for (let i = 0; i < x.length; i++) {
-      if (x[i] > 0) {
-          return i + 1;
-      }
-  }
-  
-   return x.length + 1;
-}
+  return stack.length === 0;
+};
 
-module.exports = smallestMissingPositiveInteger;
-
+module.exports = { isValid }
