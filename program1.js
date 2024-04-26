61c6344f3be57d22f9c13e59f80e8a0d3ef59627
Namanlambhate1102 <168165532+Namanlambhate1102@users.noreply.github.com>
2024-04-26 16:27:06 +0530

    complete program1.js

--- program1.js
+++ program1.js
@@ -1,24 +1,24 @@
 var isValid = function(s) {
-    const stack = [];
-    const pairs = {
-        '(': ')',
-        '[': ']',
-        '{': '}'
-    };
+  const stack = [];
+  const pairs = {
+      '(': ')',
+      '[': ']',
+      '{': '}'
+  };
 
-    for (let i = 0; i < s.length; i++) {
-        const char = s[i];
-        if (pairs[char]) {
-            stack.push(char);
-        } else {
-            const lastOpening = stack.pop();
-            if (pairs[lastOpening] !== char) {
-                return false;
-            }
-        }
-    }
+  for (let i = 0; i < s.length; i++) {
+      const char = s[i];
+      if (pairs[char]) {
+          stack.push(char);
+      } else {
+          const lastOpening = stack.pop();
+          if (pairs[lastOpening] !== char) {
+              return false;
+          }
+      }
+  }
 
-    return stack.length === 0;
+  return stack.length === 0;
 };
 
-module.exports = { isValid };
+module.exports = { isValid };
